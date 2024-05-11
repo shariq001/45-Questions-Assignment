c5c05613434b90d9c102a933edca37ba93e870a8
Muhammad Shariq <160305160+shariq001@users.noreply.github.com>
2024-05-11 22:00:31 +0500

    Add files via upload

---
+++ Q11.ts
@@ -0,0 +1,11 @@
+/*              Question 11 
+Store the names of a few of your friends in an array called names. 
+Print each person's name by accessing each element in the list, one at a time. */
+
+let friendsname:string[] = ["Adeel","Muhib","Haseeb", "Usman" ];
+        // index numbers      0        1        2         3
+console.log(friendsname[0]);
+console.log(friendsname[1]);
+console.log(friendsname[2]);
+console.log(friendsname[3]);
+// ------------------------------------------------------//
