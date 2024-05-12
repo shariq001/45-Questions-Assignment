87558671f9b6a2ba6e85a2a1a8503edbc6d1445f
Muhammad Shariq <160305160+shariq001@users.noreply.github.com>
2024-05-12 21:41:31 +0500

    Add files via upload

---
+++ Q02.ts
@@ -0,0 +1,8 @@
+/*      Question 2
+Personal Message: Store a person's name in a variable,
+ and print a message to that person. Your message should be simple
+such as, "Hello Eric", would you like to learn some python*/
+
+let person:string = "Muhammad Shariq,";
+console.log(person + " " + "How are you today?");
+// -------------------------------------------------------------//
